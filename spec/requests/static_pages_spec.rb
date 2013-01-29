00546ad3bb70586a347b5f1a2b47aea25c18ed30
Fran LP <fran.martinez@socialwin.es>
2013-01-29 16:37:36 +0100

    Finish creating static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,29 +1,57 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
 describe "StaticPages" do
 
+
   describe "Home page" do
 
-    it "should have the content 'SocialWin Analytics'" do
+    it "should have the h1 'SocialWin Analytics'" do
       visit '/static_pages/home'
-      page.should have_content('SocialWin Analytics')
+      page.should have_selector('h1', :text => 'SocialWin Analytics')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "SocialWin Analytics | Home")
+    end
+
   end
+
+
 
   describe "Help page" do
 
-    it "should have the content 'Ayuda'" do
+    it "should have the h1 'Ayuda'" do
       visit '/static_pages/help'
-      page.should have_content('Ayuda')
+      page.should have_selector('h1', :text => 'Ayuda')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "SocialWin Analytics | Ayuda")
+    end
+
   end
+
+
 
   describe "About page" do
 
-    it "should have the content 'Sobre nosotros'" do
+    it "should have the h1 'Quién somos'" do
       visit '/static_pages/about'
-      page.should have_content('Sobre nosotros')
+      page.should have_selector('h1', :text => 'Quién somos')
     end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "SocialWin Analytics | Quién somos")
+    end
+
   end
 
 
