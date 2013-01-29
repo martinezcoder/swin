f9f1202f789b87415a5d5506c5e36fc7d7c717c0
Fran LP <fran.martinez@socialwin.es>
2013-01-29 11:32:50 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swin::Application.config.secret_token = 'a82c933b46d1f37b94b405cb3c4fbd9b9ab604d1891a755ed7297042b284847d0d57ab3e494d7c581173a22eec19eed6fe28855e811f9c6309eb2ed764fb3244'
