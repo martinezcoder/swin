0c00358ee744f62d9592e7e5c5ca8afe07aefd2d
Fran LP <fran.martinez@socialwin.es>
2013-01-31 14:49:47 +0100

    user-basic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
   
   validates :name, presence: true, length: { maximum: 50 }
 
