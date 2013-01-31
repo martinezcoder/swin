d9674d6ea04f689bf07bf6b7abf7c72e84b4a55a
Fran LP <fran.martinez@socialwin.es>
2013-01-31 13:43:59 +0100

    making application user

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :name
+
+  before_save { |user| user.email = email.downcase }
+  
+  validates :name, presence: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A\w+(\w|[.]|[-]|[+])+@[a-z]([a-z]|[-]|[.])+[.][a-z]+\z/i
+  validates :email, presence:true, 
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    allow_blank: false,
+                    uniqueness: { case_sensitive: false }
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121211105959) do
+ActiveRecord::Schema.define(:version => 20130131122606) do
 
   create_table "users", :force => true do |t|
-    t.string   "provider"
-    t.string   "pid"
-    t.string   "email"
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
-    t.string   "password_digest"
     t.string   "name"
-    t.string   "remember_token"
-    t.boolean  "admin",           :default => false
-    t.string   "status"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
-  add_index "users", ["provider", "pid"], :name => "index_users_on_provider_and_pid", :unique => true
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
