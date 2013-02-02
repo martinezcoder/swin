5fd6f6942e46198e577000c3051c0562140c4efe
Fran LP <fran.martinez@socialwin.es>
2013-02-02 22:17:16 +0100

    facebook development and production apps

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, {scope: "read_stream, email, manage_pages, read_insights" }
+  if Rails.env.production?
+    provider :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, {scope: "read_stream, email, manage_pages, read_insights" }
+  else 
+    provider :facebook, FACEBOOK_APP_ID_DEV, FACEBOOK_APP_SECRET_DEV, {scope: "read_stream, email, manage_pages, read_insights" }
+  end
+    
   provider :twitter, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET
 end
