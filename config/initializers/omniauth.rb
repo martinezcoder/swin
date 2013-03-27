5bc664a6921a4ba92a337a1060f47a63d4154a7c
Fran LP <fran.martinez@socialwin.es>
2013-03-27 22:56:23 +0100

    cambios en titulos, quitar insights, scheduler task

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   if Rails.env.production?
-    provider :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, {scope: "read_stream, email, manage_pages, read_insights" }
+    provider :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, {scope: "read_stream, email, manage_pages" }
   else 
-    provider :facebook, FACEBOOK_APP_ID_DEV, FACEBOOK_APP_SECRET_DEV, {scope: "read_stream, email, manage_pages, read_insights" }
+    provider :facebook, FACEBOOK_APP_ID_DEV, FACEBOOK_APP_SECRET_DEV, {scope: "read_stream, email, manage_pages" }
   end
     
   provider :twitter, TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET
