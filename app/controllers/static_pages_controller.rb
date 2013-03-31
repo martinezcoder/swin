c44c567ed345014791fa3a5d550a911fd80df8a9
Fran LP <fran.martinez@socialwin.es>
2013-03-31 21:07:07 +0200

    users without pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   before_filter :signed_in_user, only: :habla
+  before_filter :has_active_page, only: :habla
   
   def home
     if signed_in?
@@ -13,5 +14,18 @@ class StaticPagesController < ApplicationController
 
   def habla
     session[:active] = { tab: FACEBOOK, opt: OPT_HABLA }
+    @page = current_user.pages.find_by_id(get_active_page)
   end
+
+
+private 
+
+  def has_active_page
+    begin
+      redirect_to root_path if get_active_page.nil? 
+    rescue
+      redirect_to root_path
+    end 
+  end
+  
 end
