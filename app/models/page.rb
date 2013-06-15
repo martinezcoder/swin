9e80d76ccb18127ceb9c0e014d9df80650220832
Fran LP <fran.martinez@socialwin.es>
2013-06-15 10:23:52 +0200

    eliminadas referencias antiguas a page_relationships

--- app/models/page.rb
+++ app/models/page.rb
@@ -22,14 +22,6 @@ class Page < ActiveRecord::Base
 
   has_many :user_page_relationships, foreign_key: "page_id", dependent: :destroy
   has_many :users, through: :user_page_relationships
-  
-  # what pages I consider my competitors
-  has_many :page_relationships, foreign_key: "follower_id", dependent: :destroy
-  has_many :competitors, through: :page_relationships #, source: :competitor
-
-  # pages who considers me his competitor
-  has_many :reverse_page_relationships, foreign_key: "competitor_id", class_name: "PageRelationship", dependent: :destroy
-  has_many :followers, through: :reverse_page_relationships #, source: :follower
 
   has_many :list_page_relationships, foreign_key: "page_id", class_name: "ListPageRelationship", dependent: :destroy
   has_many :lists, through: :list_page_relationships
@@ -38,26 +30,6 @@ class Page < ActiveRecord::Base
   has_many :page_data_days
 
   validates :page_id, presence: true
-
-  def following?(other_page_id)
-    self.competitors.find_by_page_id(other_page_id.to_s)
-  end
-
-  def follow!(other_page)
-    begin
-#    page_list = []
-#    page_list[0] = other_page.instance_values["attributes"]    
-#    pages_create_or_update(page_list)
-      page_relationships.create!(competitor_id: other_page.id)
-    rescue
-      nil
-    end
-  end
-
-  def unfollow!(other_page)
-    page_relationships.find_by_competitor_id(other_page.id).destroy
-  end
-
 
   def picture(big=nil)
     ret = "https://graph.facebook.com/" + page_id.to_s + "/picture"
