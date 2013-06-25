ec23bcdad8c8fd400a59d17d232d364dba2f8118
Fran LP <fran.martinez@socialwin.es>
2013-06-25 15:28:17 +0200

    top engagement acabado

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,7 +5,12 @@ class SiteController < ApplicationController
   def home 
       @pages = Page.count
       @users = User.count
-
+      
+      kk = FbMetrics.new()
+      @top_engage = FbTopEngage.where("day = ?", Time.now.yesterday.strftime("%Y%m%d").to_i)
+      @top_engage.each do |p|
+        p.engagement = kk.get_engagement(p.fan_count, p.talking_about_count)
+      end
   end
 
   def search_page
--- app/models/fb_top_engage.rb
+++ app/models/fb_top_engage.rb
@@ -1,4 +1,5 @@
 class FbTopEngage < ActiveRecord::Base
+  attr_accessor :engagement
   
   belongs_to :page
   
