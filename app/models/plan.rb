d6269e7b6ddb0942165d4d8dab6cacbe37c44fea
Fran LP <fran.martinez@socialwin.es>
2013-07-10 13:29:09 +0200

    tabla plan y tests

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -13,4 +13,11 @@
 
 class Plan < ActiveRecord::Base
 #  attr_accessible :name, :num_competitors, :num_lists, :price
+
+  validates :name, presence: true, length: { maximum: 20 },
+                   uniqueness: { case_sensitive: false }
+  validates :num_competitors, presence: true, numericality: true
+  validates :num_lists, presence: true, numericality: true
+  validates :price, presence: true, numericality: true
+  
 end
