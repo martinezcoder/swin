98e76f6baa0f0adeec865d70ccbb51a693ad083b
Fran LP <fran.martinez@socialwin.es>
2013-08-16 19:52:25 +0200

    correct app id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   if Rails.env.production?
-    # provider :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, {scope: "read_stream, email, manage_pages" }
-    provider :facebook, FACEBOOK_APP_ID_DEV, FACEBOOK_APP_SECRET_DEV, {scope: "read_stream, email, manage_pages" }
+    provider :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, {scope: "read_stream, email, manage_pages" }
   else 
     provider :facebook, FACEBOOK_APP_ID_DEV, FACEBOOK_APP_SECRET_DEV, {scope: "read_stream, email, manage_pages" }
   end
